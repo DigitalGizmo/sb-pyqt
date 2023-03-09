c1d6883cb25c285252202419e34c30c9fdce8319
Don Button <donpublic@digitalgizmo.com>
2023-03-09 13:35:37 -0500

    can't detect 2nd plugin while first is in

--- interrupt2.py
+++ interrupt2.py
@@ -10,49 +10,39 @@ from digitalio import Direction, Pull
 from RPi import GPIO
 from adafruit_mcp230xx.mcp23017 import MCP23017
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        # self.button_is_checked = True
         self.count = 0
         self.temp_window_count = 0
         self.blinking = True
         self.just_checked = False
         self.pinFlag = 15
         self.startBounceOnChange = False
-        
         self.pins_in = [False,False,False,False,False,False,False,False,False,False,False,False,False,False]
         self.names = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","Charlie","Olive","thirteen"]
-
         self.setWindowTitle("You Are the Operator")
         self.label = QLabel()
         self.label.setWordWrap(True)
         self.label.setText("Lorem")
-    
-
         self.blinkTimer=QTimer()
         self.blinkTimer.timeout.connect(self.counter)
 
         self.bounceTimer=QTimer()
         # self.bounceTimer.setSingleShot(True)
-        # self.bounceTimer.timeout.connect(continueCheckPin) moved  below continueCheckPin
+        self.bounceTimer.timeout.connect(self.continueCheckPin)
         # QTimer.singleShot(500, lambda: continueCheckPin(pin_flag))
 
         self.windowTitleChanged.connect(self.the_window_title_changed)
 
-
         # Initialize the I2C bus:
         i2c = busio.I2C(board.SCL, board.SDA)
-
-        mcp = MCP23017(i2c)
+        self.mcp = MCP23017(i2c)
 
         # Make a list of all pins
         self.pins = []
         for pinIndex in range(0, 16):
-            self.pins.append(mcp.get_pin(pinIndex))
-
+            self.pins.append(self.mcp.get_pin(pinIndex))
         # Set 0 - 7 to output
         for pinIndex in range(0, 8):
             self.pins[pinIndex].switch_to_output(value=False)
@@ -62,17 +52,16 @@ class MainWindow(QMainWindow):
             self.pins[pinIndex].direction = Direction.INPUT
             self.pins[pinIndex].pull = Pull.UP
 
-
         # Set up to check all the port B pins (pins 8-15) w/interrupts!
         # mcp.interrupt_enable = 0xFFFF  # Enable Interrupts in all pins
-        mcp.interrupt_enable = 0xFF00  # Enable Interrupts in pins 8 - 15
+        self.mcp.interrupt_enable = 0xFF00  # Enable Interrupts in pins 8 - 15
 
         # If intcon is set to 0's we will get interrupts on
         # both button presses and button releases
-        mcp.interrupt_configuration = 0x0000  # interrupt on any change
-        mcp.io_control = 0x44  # Interrupt as open drain and mirrored
+        self.mcp.interrupt_configuration = 0x0000  # interrupt on any change
+        self.mcp.io_control = 0x44  # Interrupt as open drain and mirrored
         # put this in Flask (startup)
-        mcp.clear_ints()  # Interrupts need to be cleared initially
+        self.mcp.clear_ints()  # Interrupts need to be cleared initially
 
         # connect either interrupt pin to the Raspberry pi's pin 17.
         # They were previously configured as mirrored.
@@ -82,7 +71,7 @@ class MainWindow(QMainWindow):
 
         def checkPin(port):
             """Callback function to be called when an Interrupt occurs."""
-            for pin_flag in mcp.int_flag:
+            for pin_flag in self.mcp.int_flag:
                 # print("Interrupt connected to Pin: {}".format(port))
                 print("Interrupt - pin number: {} changed to: {} ".format(pin_flag,self.pins[pin_flag].value))
                 
@@ -90,7 +79,8 @@ class MainWindow(QMainWindow):
                     # await check_pin(pin_flag)
                     startCheckPin(pin_flag)
 
-            # mcp.clear_ints()
+        GPIO.add_event_detect(interrupt, GPIO.BOTH, callback=checkPin, bouncetime=30)
+
 
         def startCheckPin(pin_flag):
             # global just_checked
@@ -108,61 +98,52 @@ class MainWindow(QMainWindow):
                 # print("setting title: %s" % new_window_title)
                 self.setWindowTitle("Window title: " + str(self.temp_window_count))
 
-        GPIO.add_event_detect(interrupt, GPIO.BOTH, callback=checkPin, bouncetime=30)
-
-
-        def continueCheckPin():
-            # print('in continueCheckPin, pin_flag: ' + str(self.pinFlag))
-            # print('in continueCheckPin, pin_flag value: ' + str(self.pins[self.pinFlag].value))
-            self.bounceTimer.stop()
-
-            print("In continue, pinFlag-3 = " + str(self.pinFlag-3) + " val: " +
-                str(self.pins[self.pinFlag-3].value))
-
-            if (self.pins[self.pinFlag-3].value == False):
-                print("Pin {} is now connected".format(self.pinFlag-3))
-
-                # print("goint to check the following pin flag minus 3: " + str(self.pinFlag))
-
-                print("Stereo pin {} aledgedly now: {}".format(self.pinFlag, self.pins[self.pinFlag].value))
-                if (self.pins[self.pinFlag].value == False):
-                    print("--- on line 2")
-                    
-                # Set pin in
-                self.pins_in[self.pinFlag-3] = True
-
-                # stop flash
-                if self.blinkTimer.isActive():
-                    self.blinkTimer.stop()
-
-                # turn this LED on
-                self.pins[self.pinFlag-11].value = True
-
-                # Send msg to screen
-                self.label.setText("Connected to {}  ".format(self.names[self.pinFlag-3]))
-
-            else:
-                # Handle case of half-plugged -- where only stereo prong engaged
-                # i.e. primary was never engaged
-                if (self.pins_in[self.pinFlag-3]):
-                    # print("-- Pin {} has been disconnected \n".format(pin_flag-3))
-                    print("-- {} has been disconnected ".format(self.names[self.pinFlag-3]))
-                    self.pins_in[self.pinFlag-3] = False
-                else:
-                    print("got to pin true, but not pin in")
-
-            print('Finished check \n')
-            mcp.clear_ints()
-
-            self.just_checked = False
-        
-        self.bounceTimer.timeout.connect(continueCheckPin)
-
-
-        self.setFixedSize(QSize(400, 300))
+        self.setFixedSize(QSize(300, 200))
         # self.setCentralWidget(container)
         self.setCentralWidget(self.label)
 
+    def continueCheckPin(self):
+        # print('in continueCheckPin, pin_flag: ' + str(self.pinFlag))
+        # print('in continueCheckPin, pin_flag value: ' + str(self.pins[self.pinFlag].value))
+        self.bounceTimer.stop()
+
+        print("In continue, pinFlag-3 = " + str(self.pinFlag-3) + " val: " +
+              str(self.pins[self.pinFlag-3].value))
+
+        if (self.pins[self.pinFlag-3].value == False):
+            print("Pin {} is now connected".format(self.pinFlag-3))
+
+            print("Stereo pin {} aledgedly now: {}".format(self.pinFlag, self.pins[self.pinFlag].value))
+            if (self.pins[self.pinFlag].value == False):
+                print("--- on line 2")
+                
+            # Set pin in
+            self.pins_in[self.pinFlag-3] = True
+
+            # stop flash
+            if self.blinkTimer.isActive():
+                self.blinkTimer.stop()
+
+            # turn this LED on
+            self.pins[self.pinFlag-11].value = True
+
+            # Send msg to screen
+            self.label.setText("Connected to {}  \n".format(self.names[self.pinFlag-3]))
+
+        else:
+            # Handle case of half-plugged -- where only stereo prong engaged
+            # i.e. primary was never engaged
+            if (self.pins_in[self.pinFlag-3]):
+                # print("-- Pin {} has been disconnected \n".format(pin_flag-3))
+                print("-- {} has been disconnected \n".format(self.names[self.pinFlag-3]))
+                self.pins_in[self.pinFlag-3] = False
+            else:
+                print("got to pin true, but not pin in")
+        
+        print("fished check \n")
+
+        # self.mcp.clear_ints()
+        self.just_checked = False
 
     def the_window_title_changed(self, window_title):
         print("window title changed so start bounceTimer: %s " % self.pinFlag)
@@ -175,7 +156,7 @@ class MainWindow(QMainWindow):
 
     def counter(self):
         self.count += 1
-        self.label.setText("count: " + str(self.count))
+        # self.label.setText("count: " + str(self.count))
         self.pins[3].value = not self.pins[3].value
         # print("Count Stereo pin {} aledgedly now: {}".format(self.pinFlag, self.pins[self.pinFlag].value))
 
