8d96722bbdeca4dfc2b71b62e2dad0892edf7dfb
Don Button <donpublic@digitalgizmo.com>
2023-08-07 13:14:25 -0400

    restart app based on control.py w gpio built in

--- app1/main.py
+++ app1/main.py
@@ -3,25 +3,26 @@ from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 
-from panel import Panel
+from panel_bak import Panel
 
 
-class MainWindow(qtw.QWidget):
+class MainWindow(qtw.QMainWindow):
 
     def __init__(self):
         """MainWindow constructor.
-
         This widget will be our main window.
         We'll define all the UI components in here.
         """
         super().__init__()
 
-        self.setLayout(qtw.QVBoxLayout())
-
+        self.setWindowTitle("You Are the Operator")
+        # self.setLayout(qtw.QVBoxLayout())
         self.label = qtw.QLabel('Keep your ears open!')
+        self.label.setAlignment(qtc.Qt.AlignTop)
+        self.setCentralWidget(self.label)
 
-        self.layout().addWidget(self.label)
-
+        # self.layout().addWidget(self.label)
+        self.setGeometry(20,120,600,200)
         self.show()
 
         # Set up panel
